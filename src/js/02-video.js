239b43abe7faa12f94a576701b97c1c71bd3038b
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-28 23:39:18 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
 
+const VIDEO_CURRENT_TIME_KEY = "videoplayer-current-time";
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+let currentTime = JSON.parse(localStorage.getItem(VIDEO_CURRENT_TIME_KEY));
+
+if (currentTime !== null) {
+  player.setCurrentTime(currentTime);
+}
+
+player.on("play", () => console.log("Player has been started"));
+player.on("timeupdate", throttle((data) => {
+  localStorage.setItem(VIDEO_CURRENT_TIME_KEY, JSON.stringify(data.seconds));
+}, 1000));
