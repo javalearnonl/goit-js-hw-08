1bbbd16a5ce2c89682521bc757089394babed387
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-28 20:03:09 +0300

    fix index page

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    textarea: document.querySelector('.feedback-form textarea')
+
+};
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextAreaInput);
+
+
+function onFormSubmit(evt) {
+    
+}
+function onTextAreaInput(evt) {
+    const value = evt.currentTarget.value;
+     console.log(value);
+
+}
