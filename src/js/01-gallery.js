6cc315de860363ee41493e6a84a5d6e5ffac0f27
maksmvc <124098393+maksmvc@users.noreply.github.com>
2023-04-28 19:06:21 +0300

    lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,17 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.css"
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryEl = document.querySelector('.gallery');
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(image => `<li><a class="gallery__item" href="${image.original}">
+                  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+                 </a></li>
+                `)
+  .join('')
+
+galleryEl.insertAdjacentHTML('afterbegin', markup)
+
+ let lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 150, spinner: true });
